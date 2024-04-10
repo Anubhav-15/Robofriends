4c944380590508660ae66f1771403f652d8346ab
AnubhavChaudhary15 <134959150+AnubhavChaudhary15@users.noreply.github.com>
2024-04-10 16:47:09 +0530

    Initial Commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({id,name,email}) => {
   return (
-    <div className='bg-light-green dib br3 ma2 pa3 bw2 shadow-5'>
+    <div className='tc grow bg-light-green dib br3 ma2 pa3 bw2 shadow-5'>
       <img src={`https://robohash.org/${id}?200x200`} alt='Not Found'/>
       <div>
         <h2>{name}</h2>
